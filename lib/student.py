011bb9d5989bf6bfcc69ef1e2d7b9474911d88ee
Steven Mentzer <stevenfmentzer@gmail.com>
2024-01-11 11:15:30 -0500

    add super

--- lib/student.py
+++ lib/student.py
@@ -4,8 +4,7 @@ from user import User
 
 class Student(User):
     def __init__(self, first_name, last_name):
-        self.first_name = first_name
-        self.last_name = last_name
+        super().__init__(first_name, last_name)
         self.knowledge = []
 
     def learn(self, string):
--- lib/teacher.py
+++ lib/teacher.py
@@ -7,9 +7,7 @@ import random
 class Teacher(User):
 
     def __init__(self, first_name, last_name):
-
-        self.first_name = first_name
-        self.last_name = last_name
+        super().__init__(first_name, last_name)
         self.knowledge = [
         "str is a data type in Python",
         "programming is hard, but it's worth it",
